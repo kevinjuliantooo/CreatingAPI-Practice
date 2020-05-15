18acc7562378648d298824147543cbc65b80ef7d
kevinjuliantooo <kevinjulianto15@gmail.com>
2020-05-15 19:24:07 +0700

    extra update

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const todos = require('./routes/todoRoutes')
+const mongoose = require('mongoose')
+
+
+let mongourl = "mongodb://localhost:27017/todoapp"
+
+const mongodb = mongourl
+mongoose.connect(mongodb)
+mongoose.Promise = global.Promise
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "Connection error coba check dulu"))
+
 const app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
 app.use("/api", todos)
+
 
 const port = 3000;
 app.listen(port, () =>
